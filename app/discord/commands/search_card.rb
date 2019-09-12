993b6b5c47da82b7fefaa63e5975f35ece4f5da2
rokhimin <rokhim.whd@gmail.com>
2019-09-12 10:55:17 +0000

    I don't know i fixed what?

--- app/discord/commands/search_card.rb
+++ app/discord/commands/search_card.rb
@@ -1,28 +1,29 @@
 module Bot::DiscordCommands
   module SearchCard   
 	  extend Discordrb::EventContainer
+            CONFIG = OpenStruct.new YAML.load_file 'config/config.yaml'
 	  
             message(description: 'searchcard') do |event|
                 card = event.message.content
 				from = /(?<=::).+(?=::)/.match(card)
 				
-                url = "https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=#{from}"
-                uri = URI(url)
-                response = Net::HTTP.get(uri)
-                atem = JSON.parse(response)
-
+				url = "#{CONFIG.api}#{from}"
+				uri = URI(url)
+				response = Net::HTTP.get(uri)
+				atem = JSON.parse(response)
+				
 				if atem[0] == nil
-						event.channel.send_embed do |embed|
-						embed.colour = 0xff1432 #red
-						embed.description = "'#{from}' not found"
-						end
+					event.channel.send_embed do |embed|
+					embed.colour = 0xff1432 #red
+					embed.description = "'#{from}' not found"
+					end
 				else
-						event.channel.send_embed do |embed|
-						embed.colour = 0xff8040 #orange
-						embed.add_field name: "name", value: "#{atem[0]["name"]}"
-						embed.add_field name: "type", value: "#{atem[0]["type"]}"
-						end
+					event.channel.send_embed do |embed|
+					embed.colour = 0xff8040 #orange
+					embed.add_field name: "name", value: "#{atem[0]["name"]}"
+					embed.add_field name: "type", value: "#{atem[0]["type"]}"
 				end
+			end
 
             end
 	  
