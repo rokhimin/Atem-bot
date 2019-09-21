728111ccaf87abfd3d7675e0ff480eb3dae0bd9c
rokhimin <rokhim.whd@gmail.com>
2019-09-21 11:03:49 +0000

    fixed menu

--- app/bot/discord/commands/general-help.rb
+++ app/bot/discord/commands/general-help.rb
@@ -9,6 +9,7 @@ module Bot::DiscordCommands
 				embed.add_field name: "**search card :**", value: "``::card_name::`` | example ``::dark magician::``"
 				embed.add_field name: "**search card (list):**", value: "``atem:src card_name`` | example ``atem:src magician``"
 				embed.add_field name: "**tier meta deck (duel links) :**", value: "``atem:dlmeta``"
+				embed.add_field name: "**random card :**", value: "``atem:random``"
 			end
 	  end
 	  
