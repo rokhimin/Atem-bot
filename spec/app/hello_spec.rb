44b7d202cd5bdcdfd11d532392d57f309b15ceed
rokhimin wahid <rokhim.whd@gmail.com>
2019-10-05 08:04:19 +0000

    hello spec

---
+++ spec/app/hello_spec.rb
@@ -0,0 +1,6 @@
+
+RSpec.describe do
+    it 'Hello Spec' do
+		expect("A").to eq("A")
+	end
+end
--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,1 +1,3 @@
-require_relative 'app/api_spec'
+
+require_relative 'app/hello_spec'
+#require_relative 'app/api_spec'
