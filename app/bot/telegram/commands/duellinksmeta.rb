20c767c2c7de65865b80fa9509ed2247c6f23373
rokhimin <rokhim.whd@gmail.com>
2019-11-01 23:38:03 +0000

    fixed duellinksmeta telegram

--- app/bot/telegram/commands/duellinksmeta.rb
+++ app/bot/telegram/commands/duellinksmeta.rb
@@ -14,7 +14,8 @@ class Duellinksmeta
 			for logic_tier1 in 1..7 do
 				main1 = url.css('div.button-row')[0]
 				works1 = main1.css('span.decktype-display')[t1_default].to_s 
-				tier1 << works1.gsub(/<\/?[^>]+>/, '')
+				checkin1 = works1.gsub(/<\/?[^>]+>/, '')
+        tier1 << checkin1 unless checkin1 == ''
 				t1_default += 1
 				count1 -= 1
 			end
@@ -26,7 +27,8 @@ class Duellinksmeta
 			for logic_tier2 in 1..7 do
 				main2 = url.css('div.button-row')[1]
 				works2 = main2.css('span.decktype-display')[t2_default].to_s 
-				tier2 << works2.gsub(/<\/?[^>]+>/, '')
+				checkin2 = works2.gsub(/<\/?[^>]+>/, '')
+        tier2 << checkin2 unless checkin2 == ''
 				t2_default += 1
 				count2 -= 1
 			end
@@ -38,12 +40,13 @@ class Duellinksmeta
 			for logic_tier3 in 1..7 do
 				main3 = url.css('div.button-row')[2]
 				works3 = main3.css('span.decktype-display')[t3_default].to_s 
-				tier3 << works3.gsub(/<\/?[^>]+>/, '')
+				checkin3 = works3.gsub(/<\/?[^>]+>/, '')
+        tier3 << checkin3 unless checkin3 == ''
 				t3_default += 1
 				count3 -= 1
 			end
 		
-		  	"*#Tier1:*\n#{tier1.join(", ")}\n*#Tier2:*\n#{tier2.join(", ")}\n*#Tier3:*\n#{tier3.join(", ")}\n-------------------\n#{date} | source : DuelLinksMeta.com"
+		  	"*#Tier1:*\n#{tier1.join(" \n")}\n*#Tier2:*\n#{tier2.join(" \n")}\n*#Tier3:*\n#{tier3.join(" \n")}\n-------------------\n#{date} | source : DuelLinksMeta.com"
 		
 	end
 end
